a40587fffaa880be718fe7114c49e899523a8f86
chemiya <jmlozanoolmedo@gmail.com>
2023-04-01 21:16:31 +0200

    inicio

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const conn = require("express-myconnection");
 const route = require("./routes/index");
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT ;
 const dbConfig = {
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || "3306",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "node-tutorial",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
 };
 
 app.use(conn(mysql, dbConfig, "single"));
